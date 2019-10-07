dc3d4cb32f28f7e28fab0b67129dcc47737b8d49
roryk <rory.kirchner@gmail.com>
2019-10-06 20:32:34 -0400

    Much slower but much simpler to follow solution.

--- marker-trimming.py
+++ marker-trimming.py
@@ -2,42 +2,49 @@ from argparse import ArgumentParser
 import pandas as pd
 import logging
 import math
+from hashlib import sha256
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     datefmt='%d-%b-%y %H:%M:%S', level=logging.DEBUG)
 
-def shrink_encoding(encoding):
-    ncols = len(next(iter(encoding)))
-    removed = []
-    new_encodings = []
-    for index in range(ncols):
-        tmpencoding = encoding.copy()
-        shrunkencoding = set()
-        for x in tmpencoding:
-            y = x[:index] + x[index+1:]
-            shrunkencoding.add(y)
-        if len(shrunkencoding) == len(encoding):
-            removed.append(index)
-            new_encodings.append(shrunkencoding)
-    return removed, new_encodings
+def find_valid_subencodings(df):
+    """
+    for an encoding dataframe df, return a list of all valid encodings
+    that result from removing a column from the dataframe
+    """
+    subencodings = []
+    for column in df.columns:
+        subencoding = df.drop([column], axis=1)
+        if is_valid_encoding(subencoding):
+            subencodings.append(subencoding)
+    return subencodings
 
-def removed_genes(removed, genes):
-    newgenes = []
-    for x in removed:
-        y = genes[:x] + genes[x+1:]
-        newgenes.append(y)
-    return(newgenes)
+def find_all_subencodings(xs):
+    """
+    for a list of encodings, return all valid subencodings
+    """
+    subencodings = []
+    for df in xs:
+        subencodings += find_valid_subencodings(df)
+    return remove_duplicate_encodings(subencodings)
 
-def shrink_encodings(encodings, genes):
-    newgenes = []
-    newencodings = []
-    for x, encoding in zip(genes, encodings):
-        removed, shrunk = shrink_encoding(encoding)
-        if removed:
-            newencodings.extend(shrunk)
-            removedgenes = removed_genes(removed, x)
-            newgenes.extend(removedgenes)
-    return newgenes, newencodings
+def is_valid_encoding(df):
+    """
+    check if a dataframe has rows that are all unique
+    """
+    # drop_duplicates removes duplicate rows
+    deduped = df.drop_duplicates()
+    return len(deduped) == len(df)
+
+def get_df_hash(df):
+    return sha256(pd.util.hash_pandas_object(df, index=True).values).hexdigest()
+
+def remove_duplicate_encodings(xs):
+    """
+    using a dataframe hash as a key, remove duplicate dataframes
+    """
+    hashdict = {get_df_hash(df): df for df in xs}
+    return hashdict.values()
 
 if __name__ == "__main__":
     parser = ArgumentParser()
@@ -45,29 +52,23 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     encodings = {}
-    data = pd.read_csv(args.markerfile)
-    with open(args.markerfile) as in_handle:
-        genes = in_handle.readline().strip().split(",")[1:]
-        for line in in_handle:
-            tokens = line.strip().split(",")
-            encodings[tokens[0]] = "".join(tokens[1:])
-
-    logging.info(f"Reducing list of markers down from {genes}.")
+    logging.info(f"Reading {args.markerfile}.")
+    data = pd.read_csv(args.markerfile, index_col="type")
+    logging.info(f"Reducing list of markers.")
     minbits = math.ceil(math.log2(len(data)))
-    logging.info(f"Minimum bits to encode all celltypes is {minbits}.")
-    bitlen = len(genes)
-    startcodes = {y for x, y in encodings.items()}
-    shrunk = [startcodes]
-    newgenes = [genes]
-    optimized = newgenes
+    logging.info(f"Minimum genes to encode all celltypes is {minbits}.")
+    encodings = [data]
+    optimized = [data]
+    round = 0
     while True:
-        if len(newgenes) > 0 and len(newgenes[0]) == minbits:
-            optimized = newgenes
+        round += 1
+        logging.info(f"Round: {round}")
+        encodings = find_all_subencodings(optimized)
+        if len(encodings) == 0:
             break
-        elif len(newgenes) == 0:
-            break
-        optimized = newgenes
-        newgenes, shrunk = shrink_encodings(shrunk, newgenes)
-    for index, y in enumerate(set([tuple(x) for x in optimized])):
-        cols = ["type"] + list(y)
-        data[cols].to_csv(f"shrunk{index}.csv", index=False)
+        optimized = encodings
+        logging.info(f"Found {len(optimized)} valid subencodings.")
+    for index, df in enumerate(optimized):
+        outfile = f"subencoding{index}.csv"
+        logging.info(f"Writing {outfile}.")
+        df.to_csv(outfile, index=True)
